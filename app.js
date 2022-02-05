48a8c2a24dcc89c903940a047ef5b587973c7400
ChanceW <chancewilliams171@gmail.com>
2022-02-05 12:11:45 -0500

    Adding Router

--- app.js
+++ app.js
@@ -1,32 +1,44 @@
 const controller = require("./controllers");
+var Router = require('router');
+var finalhandler = require('finalhandler')
+
 
 var port = process.env.PORT || 3000,
     http = require('http'),
     fs = require('fs'),
     html = fs.readFileSync('index.html');
 
+var router = new Router();
+router.route('/auth').all(function (req, res) {
+    controller.auth(req, res);
+});
+router.route('/settings').all(function (req, res) {
+    controller.settings(req, res);
+});
+router.route('/players').all(function (req, res) {
+    controller.players(req, res);
+});
+router.route('/positions').all(function (req, res) {
+    controller.positions(req, res);
+});
+router.route('/rules').all(function (req, res) {
+    controller.rules(req, res);
+});
+router.route('/resources/*').all(function (req, res) {
+    html = fs.readFileSync(req.url.substring(1));
+    res.writeHead(200);
+    res.write(html);
+    res.end();
+});
+router.route('/').all(function (req, res) {
+    html = fs.readFileSync('index.html');
+    res.writeHead(200);
+    res.write(html);
+    res.end();
+});
+
 var server = http.createServer(function (req, res) {
-    if (req.url === "/auth") {
-        controller.auth(req, res);
-    }
-    else if (req.url === "/settings") {
-        controller.settings(req, res);
-    }
-    else if (req.url === "/players") {
-        controller.players(req, res);
-    }
-    else if (req.url === "/positions") {
-        controller.positions(req, res);
-    }
-    else if (req.url === "/rules") {
-        controller.rules(req, res);
-    }
-    else {
-        html = (req.url.startsWith("/img/") || req.url.startsWith("/css/") || req.url.startsWith("/js/")) ? fs.readFileSync(req.url.substring(1)) : fs.readFileSync('index.html');
-        res.writeHead(200);
-        res.write(html);
-        res.end();
-    }
+    router(req, res, finalhandler(req, res))
 });
 
 server.listen(port);
